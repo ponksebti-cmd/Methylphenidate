3e54299926b3d161cc103ec3c93743c6f2635752
ponksebti-cmd <ponksebti@gmail.com>
2026-09-13 14:29:31 +0000

    Add Ritalin and Concerta examples

---
+++ concentra.py
@@ -0,0 +1,5 @@
+import sleep
+
+print("Taking Concerta...")
+sleep.wait(100)
+print("Focus is ready to be used.")
---
+++ ritalin.py
@@ -0,0 +1,9 @@
+import body
+import Methylphenidate
+import time
+
+patient = body.Person()
+medication = Methylphenidate.Ritalin(patient)
+print("Loading focus...")
+time.sleep(1)
+print("focus is ready to be used.")
